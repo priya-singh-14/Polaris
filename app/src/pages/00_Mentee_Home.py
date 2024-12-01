ba172e4f2ce7a298049f7c76253ebca23441e15f
priya-singh-14 <singh.priya3@northeastern.edu>
2024-11-30 19:30:46 -0500

    Set up links to Mentee Pages and renamed files to be user-story accurate

--- app/src/pages/00_Mentee_Home.py
+++ app/src/pages/00_Mentee_Home.py
@@ -9,17 +9,28 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome Political Strategist, {st.session_state['first_name']}.")
+st.title(f"Welcome to your Mentee Profile, {st.session_state['first_name']}.")
 st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('View World Bank Data Visualization', 
+if st.button('Build Profile', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/01_World_Bank_Viz.py')
+  st.switch_page('pages/01_Mentee_Profile.py')
 
-if st.button('View World Map Demo', 
+if st.button('Expand Network', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/02_Map_Demo.py')
+  st.switch_page('pages/02_Mentee_Network.py')
+
+if st.button('Apply To Jobs', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/05_Mentee_Jobs.py')
+
+if st.button('Chat', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/06_Mentee_Chats.py')
+  
