d3ed93911d4ca1cf7078c69d1e7f20f14d741b6b
priya-singh-14 <singh.priya3@northeastern.edu>
2024-12-02 18:09:55 -0500

    created a new page for employers to view the submitted applications for a specific job

---
+++ app/src/pages/44_Employer_View_Applicants.py
@@ -0,0 +1,13 @@
+import logging
+logger = logging.getLogger(__name__)
+import requests
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout = 'wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+st.title(f"Submitted Applications")
