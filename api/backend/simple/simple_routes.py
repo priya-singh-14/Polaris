936e1e545968b178a910526583ccedc79bf16f57
avakngh <166138667+avakngh@users.noreply.github.com>
2024-12-04 14:02:02 -0500

    another fix SORRY AGAIN

--- api/backend/simple/simple_routes.py
+++ api/backend/simple/simple_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response, current_app, redirect, url_for
 import json
 from backend.db_connection import db
-from backend.simple.playlist import sample_playlist_data
+# from backend.simple.playlist import sample_playlist_data
 
 # This blueprint handles some basic routes that you can use for testing
 # blueprint is a collection of routes in flask (verb and resource)
